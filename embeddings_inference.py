cab1bb1f0d645fce80c7e9384f0f1987bdc75d33
KuiMing <benjamin0901@gmail.com>
2024-02-24 09:03:30 +0800

    refactor: add cmd arguments of path and date, and rearrange the code

--- embeddings_inference.py
+++ embeddings_inference.py
@@ -1,29 +1,57 @@
-import pandas as pd
-from embeddings_save import *
+"""
+Create embedding vector for inference
+"""
+import os
 import glob
 import time
 from datetime import datetime
+import json
+import argparse
+import pandas as pd
+from llama_index import LangchainEmbedding
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from embeddings_save import save_index
 
-with open('config.json') as json_file:
-    config_dict = json.load(json_file)
-path = glob.glob("html/*")
-path.sort()
-save_directory = "~/GPT-InvestAR/inference_chroma"
-
-embedding_model = LangchainEmbedding(
+  
+def main(args) -> None:
+    """
+    main function
+    """
+    with open(args.config_path, encoding='utf8') as json_file:
+        config_dict = json.load(json_file)
+    #Read the targets df generated from make_targets.py
+    embedding_model = LangchainEmbedding(
         HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
     )
-for i in path:
-    start_time = time.time()
-    _, symbol = i.split('/')
-    path_html = glob.glob(f"html/{symbol}/*")
-    path_html.sort()
-    _, symbol, ar_date = path_html[-1].split('/')
-    save_path = os.path.join(save_directory, symbol, ar_date)
-    if int(ar_date[:4]) < datetime.now().year:
-        continue
-    if os.path.exists(save_path):
-        continue
-    save_index(save_directory, embedding_model, 
-               symbol, ar_date, config_dict)
-    print("Completed: {}, {} in {:.2f}s".format(symbol, ar_date, time.time()-start_time))
+
+    path = glob.glob("html/*/*")
+    path.sort()
+    path = pd.DataFrame({"path":path})
+    path['date'] = path.path.str.extract(r"([0-9]{4}-[0-9]{2}-[0-9]{2})")
+    date = args.date
+    if date is None:
+        date = datetime.now().strftime("%Y-%m-%d")
+    path = path[path.date >= date]
+    for i in path.iterrows():
+        # print(i[1]['path'])
+        start_time = time.time()
+        _, symbol, ar_date = i[1]['path'].split('/')
+        save_path = os.path.join(args.save_directory, symbol, ar_date)
+
+        if os.path.exists(save_path):
+            continue
+        save_index(args.save_directory, embedding_model, 
+                symbol, ar_date, config_dict)
+        print(f"Completed: {symbol}, {ar_date} in {round(time.time()-start_time, 2)}s")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', dest='config_path', type=str,
+                        required=True,
+                        help='''Full path of config.json''')
+    parser.add_argument('--save_directory', type=str, 
+                        default="/root/GPT-InvestAR/inference_chroma",
+                        required=True,
+                        help='''absolute path of chroma''')
+    parser.add_argument('--date', type=str, default=None, help="the starting date")
+    main(args=parser.parse_args())
