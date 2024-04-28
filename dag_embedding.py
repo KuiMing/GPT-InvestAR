83f5d7f94b03e6d7e3e4b95b035aac417a0f4050
KuiMing <benjamin0901@gmail.com>
2024-04-29 06:50:46 +0800

    Add DAG for getting annual report PDF and extracting embeddings

---
+++ dag_embedding.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+with DAG(
+    "get-annual-report-pdf",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A test DAG",
+    schedule="5 9 1 * *",
+    start_date=datetime(2024, 3, 6),
+    catchup=False,
+    tags=["Daily"],
+    max_active_runs=1,
+) as dag:
+
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+
+    t3 = BashOperator(
+        task_id="pdf-to-embedding",
+        depends_on_past=False,
+        bash_command="python3.9 embeddings_inference.py --config_path config.json --save_directory inference_chroma/",
+    )
+
+    t5 = BashOperator(
+        task_id="get-feature",
+        depends_on_past=False,
+        bash_command="python3.9 gpt_score_inference.py --save_directory inference_chroma/",
+    )
+
+    t3 >> t5
