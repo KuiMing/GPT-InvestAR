0f9312891e913e44ce7b322f57e33909ba1c7d80
KuiMing <benjamin0901@gmail.com>
2024-10-05 07:37:18 +0800

    chore: move the files into GPT-InvestAR folder

--- GPT-InvestAR/get_price.py
+++ GPT-InvestAR/get_price.py
@@ -6,13 +6,36 @@ import os
 import glob
 import json
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 import sqlite3
 from pandas_datareader import data as pdr
 import yfinance as yf
 import pandas as pd
 
 yf.pdr_override()
+
+
+def is_after_splits(symbol):
+    """
+    check if the stock has splits after the last update
+    """
+    stock = yf.Ticker(symbol)
+    splits = stock.splits
+    today = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+    return today in splits.index.strftime("%Y-%m-%d")
+
+
+def update_after_split(symbol, conn):
+    """
+    update the price data after splits
+    """
+    sql = f"SELECT * FROM price_table where symbol == '{symbol}'"
+    history = pd.read_sql(sql, con=conn)
+    today = datetime.today().strftime("%Y-%m-%d")
+    price_data = pdr.get_data_yahoo(symbol, start=history.Date.min()[:10], end=today)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM price_table WHERE symbol = ?", (symbol,))
+    price_data.to_sql("price_table", con=conn, if_exists="append")
 
 
 def main(args):
@@ -31,8 +54,8 @@ def main(args):
         if not data.empty:
             data.reset_index(inplace=True, drop=True)
             start_date = pd.to_datetime(data.Date)[0].strftime("%Y-%m-%d")
-
-    end_date = datetime.now().strftime("%Y-%m-%d")
+    if args.end is None:
+        end_date = datetime.now().strftime("%Y-%m-%d")
     symbol_names = [
         os.path.basename(folder)
         for folder in glob.glob(
@@ -45,6 +68,8 @@ def main(args):
         price_data = pdr.get_data_yahoo(symbol, start=start_date, end=end_date)
         price_data["symbol"] = symbol
         price_data.to_sql("price_table", con=conn, if_exists="append")
+        if is_after_splits(symbol):
+            update_after_split(symbol, conn)
 
 
 if __name__ == "__main__":
@@ -58,6 +83,11 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--start",
+        type=str,
+        help="end date",
+    )
+    parser.add_argument(
+        "--end",
         type=str,
         help="start date",
     )
